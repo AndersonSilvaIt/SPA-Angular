c3a88785cc782f9ff6b4b59e6aa53c8f0b5744c3
Anderson <anderson@prodwin.com.br>
2021-12-19 11:32:15 -0300

    Explicando a aplicação
    Explicando a aplicação

---
+++ MinhaAppAngular/src/app/funcionalidade/funcionalidade.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [
+
+  // Seus componentes
+  ],
+  imports: [
+    // Seus Módulos
+    CommonModule,
+  ],
+  providers: [
+    // Seus Serviços
+  ],
+})
+export class FuncionalidadeModule { }
